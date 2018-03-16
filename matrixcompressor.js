b8b11292e07105060115a3f56f86d53bfab96f28
Thomas Cosby <cosbyts@gmail.com>
2018-03-16 10:26:28 -0700

    Reworked matrixcompressor function to modules

--- matrixcompressor.js
+++ matrixcompressor.js
@@ -1,33 +1,4 @@
-// module.exports.compressMatrix = (matrix, key) => {
-//   let compressedMatrix = '';
-//   matrix.forEach((row, index) => {
-//     let compressedRow = '';
-//     let compressedEnd = index === matrix.length - 1 ? '' : 'row';
-//     row.forEach((value, column) => {
-//       if (value === '') {
-//         compressedRow += key;
-//       } else {
-//         compressedRow += value + key;
-//       }
-//     });
-//     compressedMatrix += compressedRow + compressedEnd;
-//   })
-//   return compressedMatrix;
-// }
-
-// module.exports.decompressMatrix = (compressedMatrix, key) => {
-//   let splitCompressed = compressedMatrix.split(',row');
-//   console.log(splitCompressed);
-//   let decompressedMatrix = splitCompressed.map((row, index) => {
-//     if (index === splitCompressed.length - 1) {
-//       return row.split(key).slice(0, -1);
-//     }
-//     return row.split(key);
-//   });
-//   return decompressedMatrix;
-// }
-
-function compressMatrix(matrix, key) {
+module.exports.compressMatrix = (matrix, key) => {
   let compressedMatrix = '';
   matrix.forEach((row, index) => {
     let compressedRow = '';
@@ -44,7 +15,7 @@ function compressMatrix(matrix, key) {
   return compressedMatrix;
 }
 
-function decompressMatrix(compressedMatrix, key) {
+module.exports.decompressMatrix = (compressedMatrix, key) => {
   let splitCompressed = compressedMatrix.split(',row');
   console.log(splitCompressed);
   let decompressedMatrix = splitCompressed.map((row, index) => {
@@ -56,51 +27,80 @@ function decompressMatrix(compressedMatrix, key) {
   return decompressedMatrix;
 }
 
-var easy = [
-  ['', '', '', '', '', '8', '', '', '4'],
-  ['', '8', '4', '', '1', '6', '', '', ''],
-  ['', '', '', '5', '', '', '1', '', ''],
-  ['1', '', '3', '8', '', '', '9', '', ''],
-  ['6', '', '8', '', '', '', '4', '', '3'],
-  ['', '', '2', '', '', '9', '5', '', '1'],
-  ['', '', '7', '', '', '2', '', '', ''],
-  ['', '', '', '7', '8', '', '2', '6', ''],
-  ['2', '', '', '3', '', '', '', '', '']
-];
+// function compressMatrix(matrix, key) {
+//   let compressedMatrix = '';
+//   matrix.forEach((row, index) => {
+//     let compressedRow = '';
+//     let compressedEnd = index === matrix.length - 1 ? '' : 'row';
+//     row.forEach((value, column) => {
+//       if (value === '') {
+//         compressedRow += key;
+//       } else {
+//         compressedRow += value + key;
+//       }
+//     });
+//     compressedMatrix += compressedRow + compressedEnd;
+//   })
+//   return compressedMatrix;
+// }
 
-var easyResult = compressMatrix(easy, ',');
+// function decompressMatrix(compressedMatrix, key) {
+//   let splitCompressed = compressedMatrix.split(',row');
+//   console.log(splitCompressed);
+//   let decompressedMatrix = splitCompressed.map((row, index) => {
+//     if (index === splitCompressed.length - 1) {
+//       return row.split(key).slice(0, -1);
+//     }
+//     return row.split(key);
+//   });
+//   return decompressedMatrix;
+// }
 
-console.log('easyResult: ', easyResult);
+// var easy = [
+//   ['', '', '', '', '', '8', '', '', '4'],
+//   ['', '8', '4', '', '1', '6', '', '', ''],
+//   ['', '', '', '5', '', '', '1', '', ''],
+//   ['1', '', '3', '8', '', '', '9', '', ''],
+//   ['6', '', '8', '', '', '', '4', '', '3'],
+//   ['', '', '2', '', '', '9', '5', '', '1'],
+//   ['', '', '7', '', '', '2', '', '', ''],
+//   ['', '', '', '7', '8', '', '2', '6', ''],
+//   ['2', '', '', '3', '', '', '', '', '']
+// ];
 
-var medium = [
-  ['', '', '', '', '9', '', '', '1', '3'],
-  ['', '', '', '5', '', '3', '', '4', ''],
-  ['9', '4', '', '', '', '', '7', '', '2'],
-  ['', '', '4', '8', '', '', '', '', ''],
-  ['6', '', '', '9', '', '2', '', '', '8'],
-  ['', '', '', '', '', '6', '4', '', ''],
-  ['3', '', '5', '', '', '', '', '2', '7'],
-  ['', '7', '', '3', '', '8', '', '', ''],
-  ['4', '6', '', '', '5', '', '', '', '']
-];
+// var easyResult = compressMatrix(easy, ',');
 
-var mediumResult = compressMatrix(medium, ',');
+// console.log('easyResult: ', easyResult);
 
-console.log('mediumResult: ', mediumResult);
+// var medium = [
+//   ['', '', '', '', '9', '', '', '1', '3'],
+//   ['', '', '', '5', '', '3', '', '4', ''],
+//   ['9', '4', '', '', '', '', '7', '', '2'],
+//   ['', '', '4', '8', '', '', '', '', ''],
+//   ['6', '', '', '9', '', '2', '', '', '8'],
+//   ['', '', '', '', '', '6', '4', '', ''],
+//   ['3', '', '5', '', '', '', '', '2', '7'],
+//   ['', '7', '', '3', '', '8', '', '', ''],
+//   ['4', '6', '', '', '5', '', '', '', '']
+// ];
 
-var hard = [
-  ['4', '', '5', '', '', '8', '', '2', ''],
-  ['', '', '', '1', '', '', '', '', ''],
-  ['', '2', '', '', '6', '7', '', '9', ''],
-  ['', '', '8', '', '', '', '', '3', ''],
-  ['5', '', '6', '', '', '', '2', '', '1'],
-  ['', '1', '', '', '', '', '4', '', ''],
-  ['', '8', '', '9', '7', '', '', '6', ''],
-  ['', '', '', '', '', '1', '', '', ''],
-  ['', '9', '', '8', '', '', '5', '', '7']
-];
+// var mediumResult = compressMatrix(medium, ',');
 
-var hardResult = compressMatrix(hard, ',');
+// console.log('mediumResult: ', mediumResult);
 
-console.log('hardResult: ', hardResult);
+// var hard = [
+//   ['4', '', '5', '', '', '8', '', '2', ''],
+//   ['', '', '', '1', '', '', '', '', ''],
+//   ['', '2', '', '', '6', '7', '', '9', ''],
+//   ['', '', '8', '', '', '', '', '3', ''],
+//   ['5', '', '6', '', '', '', '2', '', '1'],
+//   ['', '1', '', '', '', '', '4', '', ''],
+//   ['', '8', '', '9', '7', '', '', '6', ''],
+//   ['', '', '', '', '', '1', '', '', ''],
+//   ['', '9', '', '8', '', '', '5', '', '7']
+// ];
+
+// var hardResult = compressMatrix(hard, ',');
+
+// console.log('hardResult: ', hardResult);
 
