e27b2194a172d1c0c3dce2d1d4d9c8e6489a08ce
Thomas Cosby <cosbyts@gmail.com>
2018-03-15 18:36:08 -0700

    Reworked migration Promises
    - Updated scripts in the package.json to add quick scripts to migrate and rollback the database

--- database/migrations/20180315165509_create_users.js
+++ database/migrations/20180315165509_create_users.js
@@ -1,12 +1,16 @@
 
-exports.up = function(knex, Promise) {
+exports.up = (knex, Promise) => {
   return Promise.all([
-    knex.schema.createTableIfNotExists('users', function(table) {
-      table.increments(),
-      table.string('first_name', 100).nullable(),
-      table.string('last_name', 100).nullable(),
-      table.string('username', 100).notNullable().unique(),
-      table.string('email', 255).notNullable.unique()
+    knex.schema.hasTable('users').then(exists => {
+      if (!exists) {
+        return knex.schema.createTable('users', table => {
+          table.increments();
+          table.string('first_name', 100).nullable();
+          table.string('last_name', 100).nullable();
+          table.string('username', 100).notNullable().unique();
+          table.string('email', 255).notNullable().unique();
+        });
+      }
     })
   ]);
 };
--- database/migrations/20180315171418_create_puzzles.js
+++ database/migrations/20180315171418_create_puzzles.js
@@ -1,10 +1,14 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTableIfNotExists('puzzles', function(table) {
-      table.incremens(),
-      table.string('initial', 255),
-      table.string('difficulty', 100)
+    knex.schema.hasTable('puzzles').then(exists => {
+      if (!exists) {
+        return knex.schema.createTable('puzzles', table => {
+          table.increments(),
+          table.string('initial', 255),
+          table.string('difficulty', 100)
+        });
+      }
     })
   ]);
 };
